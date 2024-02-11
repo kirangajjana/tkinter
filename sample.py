352956e3d07f5d32568df806ddc7f54965819824
kirangajjana <kiran.gajjana@gmail.com>
2024-02-11 11:31:47 +0530

    updated one

--- sample.py
+++ sample.py
@@ -2,17 +2,24 @@ from tkinter import *
 window=Tk()
 def kirangajjana():
     for i in range(5):
-        print("welcome")
+        print(f"welcome")
+        if i==3:
+            print('hello kiran gajjana')
 window.geometry("420x420")
 window.title("Kiran Gajjana")
+def clickme():
+    for i in range(5):
+        print("hello welcome")
+window.geometry('420x420')
 window.config(background="skyblue")
-label=Label(text='kirangajjana',fg='red',relief=RAISED,background="gold",padx=10,pady=20)
-label.place(x=160,y=50)
+label=Label(text='kirangajjana',fg='green',background='black',font=('bold',30),relief=RAISED)
+label.pack()
 button=Button(window,
-              text="Click Me",
-              command=kirangajjana,
-              font=("arial",10),
-              fg='darkgreen',
-              bg='black')
+              text='Click Me',
+              fg='green',
+              background='gold',
+              command=clickme,
+              activeforeground='green',
+              activebackground='gold')
 button.pack()
 window.mainloop()
